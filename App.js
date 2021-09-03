76a8b10a582e0de55364335b976f4596203f3d15
defaktor <13587320019112qw>
2021-09-03 14:44:50 +0300

    fix debugTools

--- App.js
+++ App.js
@@ -7,11 +7,9 @@ import {AppNavigator} from './src/AppNavigator';
 
 const store = createStore(
   reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__())
+    : compose(applyMiddleware(thunk)),
 );
 
 export default class App extends React.Component {
